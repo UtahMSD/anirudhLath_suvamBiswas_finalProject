3ac51749dac819bf0abb002536cf219a542f6204
Anirudh Lath <anirudhlath@gmail.com>
2021-11-12 20:55:17 -0700

    Added CLION CMake Data

---
+++ cmake-build-debug-coverage/CMakeFiles/FinalProjectCS6010.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FinalProjectCS6010.dir/asteroids.obj"
+  "CMakeFiles/FinalProjectCS6010.dir/bullet.obj"
+  "CMakeFiles/FinalProjectCS6010.dir/helper.obj"
+  "CMakeFiles/FinalProjectCS6010.dir/main.obj"
+  "CMakeFiles/FinalProjectCS6010.dir/ship.obj"
+  "FinalProjectCS6010.exe"
+  "FinalProjectCS6010.exe.manifest"
+  "FinalProjectCS6010.pdb"
+  "libFinalProjectCS6010.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FinalProjectCS6010.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
